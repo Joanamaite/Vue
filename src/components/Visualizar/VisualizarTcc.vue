547768f829bffff9380e0c8cd4bb395c1b5ac8ec
Joana <joanamaite0@gmail.com>
2023-06-24 18:03:36 -0300

    Criação de cursos e alteração nas demais telas

---
+++ src/components/Visualizar/VisualizarTcc.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+     <h1 class="titulo mt-5">
+        Ferramenta para Projeto de Pesquisa 
+     </h1>
+    </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Fondamento&family=IM+Fell+French+Canon&family=Inter&display=swap');
+
+.titulo{
+    font-family: 'Fondamento', cursive;
+    margin-top: 10%;
+}
+
+</style>
