12c40e70d79591ce1c0ccb13df10e39fe435a033
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2023-06-24 17:21:47 -0300

    Ajeitando o checkbox

--- src/components/Login/PagLogin.vue
+++ src/components/Login/PagLogin.vue
@@ -1,67 +1,60 @@
 <template>
   <div class="body">
-     <div class="section">
-    <div class="container"> 
-      <div class="row full-height justify-content-center">
-        <div class="col-12 text-center align-self-center py-5">
-          <div class="section pb-5 pt-5 pt-sm-2 text-center" >
-            <div>
-             
-            </div>
-            <h6 class="mb-0 pb-3">
-              <span>Login</span>
-              <span>Cadastro</span>
-            </h6>
-            <input   class="checkbox" type="checkbox" id="reg-log" name="reg-log" v-model="checked"  />
-            <label for="reg-log"></label>
-            <div :class="['card-3d-wrap mx-auto', { active: checked }]">
-              <div class="card-3d-wrapper">
-                <div class="card-front">
-                  <div class="center-wrap">
-
-                    <div class="section text-center">
-                      <h4 class="mb-4 pb-3">Logar</h4>
-
-                      <div class="form-group">
-                        <input type="email" name="logemail" class="form-style" placeholder="Seu E-mail" id="logemail" autocomplete="off" v-model="loginEmail" />
-                        <i class="input-icon uil uil-at"></i>
+    <div class="section">
+      <div class="container">
+        <div class="row full-height justify-content-center">
+          <div class="col-12 text-center align-self-center py-5">
+            <div class="section pb-5 pt-5 pt-sm-2 text-center">
+              <div></div>
+              <h6 class="mb-0 pb-3">
+                <span>Login</span>
+                <span>Cadastro</span>
+              </h6>
+              <input class="checkbox" type="checkbox" id="reg-log" name="reg-log" v-model="checked" style="display: none;" />
+              <label for="reg-log"></label>
+              <div :class="['card-3d-wrap mx-auto', { active: checked }]">
+                <div class="card-3d-wrapper">
+                  <div class="card-front">
+                    <div class="center-wrap">
+                      <div class="section text-center">
+                        <h4 class="mb-4 pb-3">Logar</h4>
+                        <div class="form-group">
+                          <input type="email" name="logemail" class="form-style" placeholder="Seu E-mail" id="logemail" autocomplete="off" v-model="loginEmail" />
+                          <i class="input-icon uil uil-at"></i>
+                        </div>
+                        <div class="form-group mt-2">
+                          <input type="password" name="logpass" class="form-style" placeholder="Sua Senha" id="logpass" autocomplete="off" v-model="loginPassword" />
+                          <i class="input-icon uil uil-lock-alt"></i>
+                        </div>
+                        <div>
+                          <a @click="Home" class="btn mt-4">Enviar</a>
+                        </div>
+                        <p class="mb-0 mt-4 text-center"><a href="" class="link">Esqueceu sua senha?</a></p>
                       </div>
-                      
-                      <div class="form-group mt-2">
-                        <input type="password" name="logpass" class="form-style" placeholder="Sua Senha" id="logpass" autocomplete="off" v-model="loginPassword" />
-                        <i class="input-icon uil uil-lock-alt"></i>
-                      </div>
-                 
-                      <div>
-                        <a @click="Home" class="btn mt-4">Enviar</a>
-                      </div>
-                      <p class="mb-0 mt-4 text-center"><a href="" class="link">Esqueceu sua senha?</a></p>
                     </div>
                   </div>
-                </div>
-
-                <div class="card-back">
-                  <div class="center-wrap">
-                    <div class="section text-center">
-                      <h4 class="mb-4 pb-3">Cadastrar</h4>
-                      <div class="form-group" style="margin-top: 20px;">
-                        <input type="text" name="name" class="form-style" placeholder="Nome" id="name" autocomplete="off" v-model="name" />
-                        <i class="input-icon uil uil-user"></i>
+                  <div class="card-back">
+                    <div class="center-wrap">
+                      <div class="section text-center">
+                        <h4 class="mb-4 pb-3">Cadastrar</h4>
+                        <div class="form-group" style="margin-top: 20px;">
+                          <input type="text" name="name" class="form-style" placeholder="Nome" id="name" autocomplete="off" v-model="name" />
+                          <i class="input-icon uil uil-user"></i>
+                        </div>
+                        <div class="form-group" style="margin-top: 20px;">
+                          <input type="text" name="email" class="form-style" placeholder="E-mail" id="email" autocomplete="off" v-model="email" />
+                          <i class="input-icon uil uil-envelope"></i>
+                        </div>
+                        <div class="form-group" style="margin-top: 20px;">
+                          <input type="password" name="password" class="form-style" placeholder="Senha" id="password" autocomplete="off" v-model="password" />
+                          <i class="input-icon uil uil-lock-alt"></i>
+                        </div>
+                        <div class="form-group" style="margin-top: 20px;">
+                          <input type="password" name="confirmPassword" class="form-style" placeholder="Confirmar Senha" id="confirmPassword" autocomplete="off" v-model="confirmPassword" />
+                          <i class="input-icon uil uil-lock-alt"></i>
+                        </div>
+                        <a href="#" class="btn mt-4">Enviar</a>
                       </div>
-                      <div class="form-group" style="margin-top: 20px;">
-                        <input type="text" name="email" class="form-style" placeholder="E-mail" id="email" autocomplete="off" v-model="email" />
-                        <i class="input-icon uil uil-envelope"></i>
-                      </div>
-                      <div class="form-group" style="margin-top: 20px;">
-                        <input type="password" name="password" class="form-style" placeholder="Senha" id="password" autocomplete="off" v-model="password" />
-                        <i class="input-icon uil uil-lock-alt"></i>
-                      </div>
-                      <div class="form-group" style="margin-top: 20px;">
-                        <input type="password" name="confirmPassword" class="form-style" placeholder="Confirmar Senha" id="confirmPassword" autocomplete="off" v-model="confirmPassword" />
-                        <i class="input-icon uil uil-lock-alt"></i>
-                      </div>
-                     
-                      <a href="#" class="btn mt-4">Enviar</a>
                     </div>
                   </div>
                 </div>
@@ -72,9 +65,9 @@
       </div>
     </div>
   </div>
-  </div>
 </template>
 
+<!--Começo da Lógica-->
 <script>
 export default {
   data() {
@@ -104,6 +97,7 @@ export default {
 };
 </script>
 
+<!--Começo da Estilização-->
 <style scoped>
 @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800,900');
 .body{
@@ -152,9 +146,6 @@ h6 span{
 .full-height{
   min-height: 100vh;
 }
-
-
-
 .checkbox:checked + label,
 .checkbox:not(:checked) + label{
   position: relative;
