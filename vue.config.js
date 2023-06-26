32b7034bd374524fa384c317707224079b6287fc
Joana <joanamaite0@gmail.com>
2023-06-25 23:43:55 -0300

    ajustes

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
-})
-const path = require('path');
-module.exports = {
+  ],
   chainWebpack: (config) => {
     config.resolve.alias.set('@', path.resolve(__dirname, 'src'));
   },
-};
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Vue/'
-    : '/'
-};
-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
+  publicPath: process.env.NODE_ENV === 'production' ? '/Vue/' : '/',
+  outputDir: 'dist',
+  // outras configurações...
+});
