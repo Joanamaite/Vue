6c26cd61f88df9a8c9ca4ccfe5dfcc65399f6fb0
Joana <joanamaite0@gmail.com>
2023-06-25 23:25:43 -0300

    ajustando rotas

--- vue.config.js
+++ vue.config.js
@@ -10,3 +10,10 @@ module.exports = {
     config.resolve.alias.set('@', path.resolve(__dirname, 'src'));
   },
 };
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Vue/'
+    : '/'
+};
+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
